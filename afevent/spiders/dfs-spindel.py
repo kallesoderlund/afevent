e16e8934b05c631a04875bbc7f3eed53f5d6ccd3
kalle <kallesoderlund@gmail.com>
2016-03-07 13:50:53 +0100

    Nya DFS

--- afevent/spiders/dfs-spindel.py
+++ afevent/spiders/dfs-spindel.py
@@ -10,7 +10,8 @@ class myItems(scrapy.Item):
 	city = scrapy.Field()
 	host = scrapy.Field()
 	date = scrapy.Field()
-	link = scrapy.Field()
+	time = scrapy.Field()
+	url = scrapy.Field()
 
 
 class MySpider(Spider):
@@ -26,13 +27,14 @@ class MySpider(Spider):
 		title_list = divs.xpath('./tr/td/div[@class = "event-title"]/span[@class = "link"]/text()').extract()
 		city_list = divs.xpath('./tr/td[4]/text()').extract()
 		host_list = divs.xpath('./tr/td[5]/a/text()').extract()
-		date_list = divs.xpath('./tr/td[1]/span/text()').extract()
-		link_list = divs.xpath('./tr/td[3]/div[2]/a/@href').extract()
+		date_time_list = divs.xpath('./tr/td[1]/span/text()').extract()
+		url_list = divs.xpath('./tr/td[3]/div[2]/a/@href').extract()
 
 		#Trim the data
 		city_list = [y.strip() for y in city_list]
-		date_list = [y.split(' ') for y in date_list]
-		link_list = ['https://natverk.dfs.se' + y for y in link_list]
+		date_time_list = [y.split(' ') for y in date_time_list]
+		date_list, time_list = map(list, zip(*date_time_list))
+		url_list = ['https://natverk.dfs.se' + y for y in url_list]
 
 		#Combine related attributes into events
 		for x in range(0,len(title_list)):
@@ -40,6 +42,7 @@ class MySpider(Spider):
 			item['city'] = city_list[x]
 			item['host'] = host_list[x]
 			item['date'] = date_list[x]
-			item['link'] = link_list[x]
+			item['time'] = time_list[x]
+			item['url'] = url_list[x]
 			yield item
 		
